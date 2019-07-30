112f9af7ab80928cd131183cb28b0332a4228e1a
zhanzehao <1935582426@qq.com>
2019-07-30 16:58:44 +0800

    add controller

--- gaoxi-controller/src/main/java/com/zhanzeh/gaoxicontroller/controller/UserControllerImpl.java
+++ gaoxi-controller/src/main/java/com/zhanzeh/gaoxicontroller/controller/UserControllerImpl.java
@@ -2,6 +2,7 @@ package com.zhanzeh.gaoxicontroller.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zhanzeh.gaoxicommonservicefacade.api.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,7 @@ public class UserControllerImpl implements UserController {
     @Reference(version = "1.0.0")
     private UserService userService;
 
+    @GetMapping("/login")
     public String login(String username,String password) {
         return userService.login(username,password);
     }
--- gaoxi-user/src/main/java/com/zhanzeh/gaoxiuser/service/UserServiceImpl.java
+++ gaoxi-user/src/main/java/com/zhanzeh/gaoxiuser/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.zhanzeh.gaoxicommonservicefacade.api.UserService;
 
 @Service(version = "1.0.0")
+@org.springframework.stereotype.Service
 public class UserServiceImpl implements UserService {
 
     public String login(String username,String password) {
